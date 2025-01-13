6d9059f6b1fa20cb72ca72db92c834256d4d12b4
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2025-01-12 22:37:47 -0300

    API Escola com autenticação básica. Estudantes, Cursos e Matrículas.

--- escola/models.py
+++ escola/models.py
@@ -20,7 +20,17 @@ class Curso(models.Model):
     )
     codigo = models.CharField(max_length = 10)
     descricao = models.CharField(max_length = 100, blank = False)
-    nivel = models.CharField(max_length = 1, choices=NIVEL, blank = False, null = False)
+    nivel = models.CharField(max_length = 1, choices=NIVEL, blank = False, null = False, default = 'B')
 
     def __str__(self):
         return self.codigo
+
+class Matricula(models.Model):
+    PERIODO = (
+        ('M','Manhã'),
+        ('T','Tarde'),
+        ('N','Noite'),
+    )
+    estudante = models.ForeignKey(Estudante, on_delete = models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete = models.CASCADE)
+    periodo = models.CharField(max_length = 1, choices=PERIODO, blank = False, null = False, default = 'M')
--- escola/views.py
+++ escola/views.py
@@ -1,9 +1,31 @@
-from django.http import JsonResponse
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import (EstudanteSerializer, 
+                                CursoSerializer, 
+                                MatriculaSerializer, 
+                                ListaMatriculasEstudanteSerializar, 
+                                ListaMatriculasCursoSerializar)
+from rest_framework import viewsets, generics
 
-def estudantes(request):
-    if request.method == 'GET':
-        estudante = {
-            'id': '1',
-            'nome': 'fernando'
-        }
-        return JsonResponse(estudante)
+class EstudanteViewSet(viewsets.ModelViewSet):
+    queryset = Estudante.objects.all()
+    serializer_class = EstudanteSerializer
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasEstudate(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializar
+
+class ListaMatriculasCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializar
