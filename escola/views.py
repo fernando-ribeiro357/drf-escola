305e9fb4fc7481037f76ce5057339b762a6171ba
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2025-03-31 00:15:48 -0300

    Adicionado middleware para CORS.

--- escola/views.py
+++ escola/views.py
@@ -6,11 +6,31 @@ from escola.serializers import (EstudanteSerializer,
                                 ListaMatriculasEstudanteSerializer, 
                                 ListaMatriculasCursoSerializer)
 from rest_framework import viewsets, generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.throttling import UserRateThrottle
 from escola.throttles import MatriculaAnonRateThrottle
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class EstudanteViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de estudantes.
+
+    Campos de ordenação:
+    - nome: permite ordenar os resultados por nome.
+
+    Campos de pesquisa:
+    - nome: permite pesquisar os resultados por nome.
+    - cpf: permite pesquisar os resultados por CPF.
+    - email: permite pesquisar os resultados por email.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+
+    Classe de Serializer:
+    - EstudanteSerializer: usado para serialização e desserialização de dados.
+    - Se a versão da API for 'v2', usa EstudanteSerializerV2.
+    """
     queryset = Estudante.objects.all().order_by("id")
     # serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
@@ -22,24 +42,56 @@ class EstudanteViewSet(viewsets.ModelViewSet):
         return EstudanteSerializer
 
 class CursoViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de cursos.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+    """
     queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering_fields = ['codigo']
+    search_fields = ['codigo', 'descricao']
 
 class MatriculaViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de matrículas.
+
+    Métodos HTTP Permitidos:
+    - GET, POST
+
+    Throttle Classes:
+    - MatriculaAnonRateThrottle: limite de taxa para usuários anônimos.
+    - UserRateThrottle: limite de taxa para usuários autenticados.
+    """
     queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer
     throttle_classes = [UserRateThrottle, MatriculaAnonRateThrottle]
     http_method_names = ["get","post"]
     
 class ListaMatriculasEstudante(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista de Matrículas por id de Estudante
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculasCurso(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista de Matrículas por id de Curso
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by("id")
         return queryset
--- setup/urls.py
+++ setup/urls.py
@@ -26,7 +26,7 @@ schema_view = get_schema_view(
       title="Documentação da API",
       default_version='v1',
       description="Documentação da API Escola",
-      terms_of_service="https://www.google.com/policies/terms/",
+      terms_of_service="#",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
    ),
@@ -44,5 +44,5 @@ urlpatterns = [
     path('estudantes/<int:pk>/matriculas/',ListaMatriculasEstudante.as_view()),
     path('cursos/<int:pk>/matriculas/',ListaMatriculasCurso.as_view()),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
